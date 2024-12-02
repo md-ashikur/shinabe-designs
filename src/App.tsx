5dcc2afe52904b7a2f9c6265e1cc5d850b9d593e
Ekbal41 <mdasifekbalshagor@gmail.com>
2024-12-02 14:02:27 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen">
+        <div className="max-w-7xl mx-auto bg-black min-h-screen">
+          <Hero />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
